887f71cc2d9508dd6bb7a5373689941052c70817
DESKTOP-OHUMN8M\OddCommon2 <rfw7759@gmail.com>
2023-08-31 14:21:59 -0400

    oddcommon commit

--- Assets/Content/SlipStreamII/Game_Ship.cs
+++ Assets/Content/SlipStreamII/Game_Ship.cs
@@ -8,23 +8,19 @@ public class Game_Ship : MonoBehaviour
     [SerializeField] LineRenderer line;
     [SerializeField] Transform LineStart;
     [SerializeField] Transform LineEnd;
-
     [SerializeField] Camera gameCam;
     [SerializeField] Transform Ta;
     [SerializeField] Transform ShipNameText;
 
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
- 
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         line.SetPosition(0, LineStart.position);
         line.SetPosition(1, LineEnd.position);
 
@@ -32,13 +28,7 @@ public class Game_Ship : MonoBehaviour
         {
             Vector3 Loc = Ta.position - ShipNameText.position;
             Quaternion rot = Quaternion.LookRotation(Loc, Vector3.up);
-            ShipNameText.rotation = rot;
-           
-           
-
-
-        }
-       
-
+            ShipNameText.rotation = rot;       
+        }    
     }
 }
